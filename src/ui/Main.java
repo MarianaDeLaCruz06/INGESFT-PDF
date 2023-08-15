3e73c7e18e7bad32dbddbecaaa21de9ac684ec36
Milxxx18 <juancamiloamorochor@hotmail.com>
2023-08-15 13:41:10 -0500

    Entrega Juan Camilo

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,13 +2,9 @@ package ui;
 
 import java.util.Scanner;
 
-
-
 public class Main{
+
   public static Scanner reader = new Scanner(System.in);
-
-
-    
     
     public static void main(String[] args){
         
